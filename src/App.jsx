ece4273689fb6e6bff9957d9d5e9a247308d707d
xMathijs <161062972+xMathijs@users.noreply.github.com>
2024-04-28 10:52:56 +0000

    ''how to'' added in react template

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn how to React
           </a>
         </p>
       </header>
